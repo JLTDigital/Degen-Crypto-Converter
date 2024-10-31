6cde769f0a53a8339c06d095a9a7bd48554f3365
JLTDigital <mrjltaft@gmail.com>
2024-10-31 21:59:11 +0000

    Initial Style

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
 const App = () => {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+  return (
+    <h1 className='text-3xl font-bold text-white underline'>Hello world!</h1>
+  )
 }
 
 export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      display: ['Tiny5', 'sans-serif']
+    },
     extend: {}
   },
   plugins: []
