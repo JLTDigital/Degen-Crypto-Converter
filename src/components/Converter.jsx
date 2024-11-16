2cc230e92af1739e06e6aeb8b0a6c21e03db640e
JLTDigital <mrjltaft@gmail.com>
2024-11-16 17:38:50 +0000

    update

--- src/components/Converter.jsx
+++ src/components/Converter.jsx
@@ -11,7 +11,7 @@ const Converter = ({ title }) => {
   console.log(dataFromChild)
 
   return (
-    <div className='text-center p-4 grid grid-cols-2 justify-center items-center flex-col'>
+    <div className='text-center p-4 grid md:grid-cols-2 grid-cols-1 justify-center items-center flex-col'>
       <div>
         <h2 className='text-p1 text-center text-2xl mb-4'>{title}</h2>
         <div className='text-white rounded-xl border border-gray-100 w-full grid grid-cols-1 bg-black opacity-80'>
