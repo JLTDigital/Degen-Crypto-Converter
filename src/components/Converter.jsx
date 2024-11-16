43aed8938c048057064170d7aa495624f74eeb82
JLTDigital <mrjltaft@gmail.com>
2024-11-16 18:21:24 +0000

    Update

--- src/components/Converter.jsx
+++ src/components/Converter.jsx
@@ -1,23 +1,31 @@
 import React, { useState } from 'react'
 import CryptoConverter from './CryptoConverter'
+import Info from './Info'
 
 const Converter = ({ title }) => {
-  const [dataFromChild, setDataFromChild] = useState({})
+  const [cryptoInfo, setCryptoInfo] = useState({})
+  const [hidden, setHidden] = useState(true)
 
   const handleDataFromChild = (infoData) => {
-    setDataFromChild(infoData)
+    setCryptoInfo(infoData)
+    setHidden(false)
   }
-
-  console.log(dataFromChild)
 
   return (
     <div className='text-center p-4 grid md:grid-cols-2 grid-cols-1 justify-center items-center flex-col'>
-      <div>
+      <div className='container'>
         <h2 className='text-p1 text-center text-2xl mb-4'>{title}</h2>
         <div className='text-white rounded-xl border border-gray-100 w-full grid grid-cols-1 bg-black opacity-80'>
           <CryptoConverter sendDataToParent={handleDataFromChild} />
         </div>
       </div>
+      {hidden === true ? (
+        <div className='container text-center justify-center hidden'></div>
+      ) : (
+        <div className='container text-center justify-center'>
+          <Info cryptoInfo={cryptoInfo} />
+        </div>
+      )}
     </div>
   )
 }
---
+++ src/components/Info.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Info = ({ cryptoInfo }) => {
+  return (
+    <div className='info-holder'>
+      <div className='flex justify-center'>
+        <img className='w-1/4' src={cryptoInfo.image} alt={cryptoInfo.name} />
+      </div>
+      <ul className='text-white'>
+        <li>{cryptoInfo.symbol}</li>
+        <br />
+        <li>Name: {cryptoInfo.name}</li>
+        <li>Current Price: {cryptoInfo.currentPrice}</li>
+        <li>Market Cap: {cryptoInfo.marketCap}</li>
+        <li>Market Cap Rank: {cryptoInfo.marketCapRank}</li>
+        <br />
+        <li>24hr High: {cryptoInfo.high24h}</li>
+        <li>24hr Low: {cryptoInfo.low24h}</li>
+        <li>24hr Price Change: {cryptoInfo.priceChange24h}</li>
+        <li>24hr Price Percentage: {cryptoInfo.priceChangePercentage24h}</li>
+        <li>Circulating Supply: {cryptoInfo.circulatingSupply}</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Info
